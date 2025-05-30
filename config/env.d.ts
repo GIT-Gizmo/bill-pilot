46676a0f78f8b93d5b9c5ec1c968a1287aef8e3c
GIT-Gizmo <boluforjesus@gmail.com>
2025-05-30 21:14:47 +0100

    feat: add env.d.ts for environment variable types - Introduced env.d.ts to define TypeScript types for environment variables including PORT, NODE_ENV, DB_URI, JWT_SECRET, and JWT_EXPIRES_IN.

---
+++ config/env.d.ts
@@ -0,0 +1,6 @@
+// config/env.d.ts
+export const PORT: number | undefined;
+export const NODE_ENV: string | undefined;
+export const DB_URI: string | undefined;
+export const JWT_SECRET: string | undefined;
+export const JWT_EXPIRES_IN: number | undefined;
